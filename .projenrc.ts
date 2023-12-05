af735cc48bbfe3ed2071ece0ebc77989f019f34d
Masatoshi Higuchi <2480630+ogis-higuchi@users.noreply.github.com>
2023-12-05 05:44:29 +0000

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,36 @@
+import { awscdk, javascript } from 'projen';
+
+const project = new awscdk.AwsCdkConstructLibrary({
+  name: '@ogis-rd/awscdk-nat-lib',
+  description: 'AWS CDK constructs for NAT devices',
+
+  buildWorkflow: false,
+  depsUpgradeOptions: {
+    workflow: false,
+  },
+  githubOptions: {
+    pullRequestLint: false,
+  },
+  pullRequestTemplate: false,
+  releaseTrigger: {
+    isManual: true,
+    isContinuous: false,
+  },
+
+  // The oldest version to import `aws-cdk-lib/core` for running `npm test`
+  // See https://github.com/aws/aws-cdk/pull/24376
+  cdkVersion: '2.73.0',
+  jsiiVersion: '~5.0.0',
+
+  packageManager: javascript.NodePackageManager.NPM,
+  projenrcTs: true,
+
+  repositoryUrl: 'https://github.com/ogis-rd/awscdk-nat-lib.git',
+  defaultReleaseBranch: 'main',
+
+  author: 'OGIS-RI Co.,Ltd.',
+  authorAddress: 'https://www.ogis-ri.co.jp',
+  authorOrganization: true,
+});
+
+project.synth();
---
+++ README.md
@@ -0,0 +1,5 @@
+# awscdk-nat-lib
+
+## License
+
+This project is licensed under the [Apache-2.0](LICENSE) license.
