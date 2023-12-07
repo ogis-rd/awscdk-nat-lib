e8debd050ddd05637eac3d4153a59571565b14bf
Masatoshi Higuchi <2480630+ogis-higuchi@users.noreply.github.com>
2023-12-07 06:13:11 +0000

    feat(eip): add Eip construct

---
+++ src/eip.ts
@@ -0,0 +1,91 @@
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as core from 'aws-cdk-lib/core';
+import { Construct } from 'constructs';
+import { checkTypeBySymbol } from './util';
+
+const EIP_SYMBOL = Symbol.for('@ogis-rd/awscdk-nat-lib.Eip');
+
+/**
+ * An Elastic IP (EIP) address.
+ */
+export interface IEip extends core.IResource {
+  /**
+   * The allocation ID.
+   *
+   * @attribute
+   */
+  readonly eipAllocationId: string;
+
+  /**
+   * The Elastic IP address.
+   *
+   * @attribute
+   */
+  readonly eipPublicIp: string;
+}
+
+/**
+ * Attributes used to import an EIP.
+ */
+export interface EipAttributes {
+  /**
+   * The allocation ID.
+   */
+  readonly eipAllocationId: string;
+
+  /**
+   * The Elastic IP address.
+   */
+  readonly eipPublicIp?: string;
+}
+
+abstract class EipBase extends core.Resource implements IEip {
+  public abstract readonly eipAllocationId: string;
+  public abstract readonly eipPublicIp: string;
+}
+
+/**
+ * An Elastic IP (EIP) address.
+ *
+ * @resource AWS::EC2::EIP
+ */
+export class Eip extends EipBase {
+  /**
+   * Import an unowned EIP from attributes.
+   */
+  public static fromEipAttributes(scope: Construct, id: string, attrs: EipAttributes): IEip {
+    class Import extends EipBase {
+      public readonly eipAllocationId = attrs.eipAllocationId;
+      private readonly _eipPublicIp = attrs.eipPublicIp;
+
+      public get eipPublicIp() {
+        if (!this._eipPublicIp) {
+          throw new Error('eipPublicIp is not provided for the imported Eip.');
+        }
+        return this._eipPublicIp;
+      };
+    }
+    return new Import(scope, id);
+  }
+
+  /**
+   * Return true if the object is an EIP.
+   */
+  public static isEip(x: any): x is Eip {
+    return checkTypeBySymbol(x, EIP_SYMBOL);
+  }
+
+  public readonly eipAllocationId: string;
+  public readonly eipPublicIp: string;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    Object.defineProperty(this, EIP_SYMBOL, { value: true });
+
+    const eip = new ec2.CfnEIP(this, 'Resource');
+
+    this.eipAllocationId = eip.attrAllocationId;
+    this.eipPublicIp = eip.attrPublicIp;
+  }
+}
---
+++ test/eip.test.ts
@@ -0,0 +1,57 @@
+import { Template } from 'aws-cdk-lib/assertions';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as core from 'aws-cdk-lib/core';
+import { Eip, EipAttributes } from '../src';
+
+describe('EIP', () => {
+  let stack: core.Stack;
+
+  beforeEach(() => {
+    stack = new core.Stack();
+  });
+
+  test('default', () => {
+    const eip = new Eip(stack, 'Eip');
+
+    expect(Eip.isEip(eip)).toBe(true);
+    expect(eip.node.defaultChild instanceof ec2.CfnEIP).toBe(true);
+
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs('AWS::EC2::EIP', 1);
+    template.resourcePropertiesCountIs('AWS::EC2::EIP', {}, 1);
+  });
+
+  test('import from attributes', () => {
+    const attributes: EipAttributes = {
+      eipAllocationId: 'eipalloc-123456789abcdefgh',
+      eipPublicIp: '1.2.3.4',
+    };
+    const eip = Eip.fromEipAttributes(stack, 'Eip', attributes);
+
+    expect(Eip.isEip(eip)).toBe(false);
+    expect(eip.eipAllocationId).toBe(attributes.eipAllocationId);
+    expect(eip.eipPublicIp).toBe(attributes.eipPublicIp);
+
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs('AWS::EC2::EIP', 0);
+  });
+
+  test('import without eipPublicIp', () => {
+    const attributes: EipAttributes = {
+      eipAllocationId: 'eipalloc-123456789abcdefgh',
+    };
+    const eip = Eip.fromEipAttributes(stack, 'Eip', attributes);
+
+    expect(Eip.isEip(eip)).toBe(false);
+    expect(eip.eipAllocationId).toBe(attributes.eipAllocationId);
+    expect(() => {
+      eip.eipPublicIp;
+    }).toThrow('eipPublicIp is not provided for the imported Eip.');
+
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs('AWS::EC2::EIP', 0);
+  });
+});
