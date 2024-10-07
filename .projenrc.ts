2824a7ab0856cca4a4e67a4d14e4b2d34b34453c
Masatoshi Higuchi <2480630+ogis-higuchi@users.noreply.github.com>
2024-10-07 02:04:22 +0000

    chore(deps): bump projen from 0.79.27 to 0.87.4

--- .projenrc.ts
+++ .projenrc.ts
@@ -21,7 +21,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   // CVE-2023-35165 has been fixed in the version
   cdkVersion: '2.80.0',
   jsiiVersion: '~5.3.0',
-  projenVersion: '0.79.27',
+  projenVersion: '0.87.4',
   // Aligned with jsii 5.3.46 (build 7ede562)
   typescriptVersion: '5.3.3',
 
