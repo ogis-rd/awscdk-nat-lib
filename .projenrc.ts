46a8e2bc463ae604ccdb09a89f9b4993a1968b43
Masatoshi Higuchi <2480630+ogis-higuchi@users.noreply.github.com>
2024-10-08 04:43:43 +0000

    chore(github): add pull-request-lint workflow and pull_request_template.md

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,5 +1,7 @@
 import { awscdk, javascript } from 'projen';
 
+const contributorStatement = 'By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license.';
+
 const project = new awscdk.AwsCdkConstructLibrary({
   name: '@ogis-rd/awscdk-nat-lib',
   description: 'AWS CDK constructs for NAT devices',
@@ -9,9 +11,22 @@ const project = new awscdk.AwsCdkConstructLibrary({
   },
   githubOptions: {
     mergify: false,
-    pullRequestLint: false,
+    pullRequestLintOptions: {
+      contributorStatement,
+      semanticTitleOptions: {
+        types: [
+          'chore',
+          'docs',
+          'feat',
+          'fix',
+        ],
+      },
+    },
   },
-  pullRequestTemplate: false,
+  pullRequestTemplateContents: [
+    // empty for now
+    '',
+  ],
   releaseTrigger: {
     isManual: false,
     isContinuous: false,
